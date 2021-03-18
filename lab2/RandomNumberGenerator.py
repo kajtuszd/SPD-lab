f55194c399e5483bc1a4b529a54cc7c6c467b5f4
kajetan <kajetanzdanowicz@wp.pl>
2021-03-18 14:55:55 +0100

    schrage_ptmn not working well

--- lab2/RandomNumberGenerator.py
+++ lab2/RandomNumberGenerator.py
@@ -69,6 +69,40 @@ def Schrage(tasks, rj, pj, qj):
     return tasks
 
 
+
+def Schrage_ptmn(tasks, rj, pj, qj):
+    Ng = []
+    tasks = list(tasks)
+    Nn = tasks
+    tasks = []
+    l = 0 
+    q0 = 99999999999
+    t = 0
+    Cmax = 0
+    while (len(Ng)) or (len(Nn)):
+        while (len(Nn)) and (min(rj) <= t):
+            j = rj.index(min(rj))
+            rj[j] = 99999999999
+            Ng.append(j + 1)
+            Nn.remove(j + 1)
+            if qj[j+1] > qj[l]:
+                pj[l] = t - rj[j+1]
+                t = rj[j+1]
+                if pj[l] > 0:
+                    Ng.append(j + 1)            
+        if len(Ng):
+            j = qj.index(max([qj[i - 1] for i in Ng])) + 1
+            qj[j - 1] = - 99999999999
+            Ng.remove(j)
+            l = j # +1 ??
+            t += pj[j - 1]
+            Cmax = max(Cmax, t + qj[j - 1])
+        else:
+            t = min(rj)
+    return tasks
+
+
+
 def main():
     seed = int(input("Enter Z number: "))
     generator = RandomNumberGenerator(seed)
@@ -106,6 +140,16 @@ def main():
     [[Sj, Cj, Cjq], Cmax] = calculate([row[1] for row in Tab], [row[2] for row in Tab], [row[3] for row in Tab], task_number)
     print("\npi: {} \nS: {} \nC: {} \nCq: {}".format(pi, Sj, Cj, Cjq))
     print("Cmax: {}".format(Cmax))
+
+    print("\n\n ____________ \n")
+    pi = Schrage_ptmn(tasks, rj, pj, qj)
+    sort = {x: i for i, x in enumerate(pi)}
+    Tab.sort(key = lambda x: sort[x[0]])
+
+    [[Sj, Cj, Cjq], Cmax] = calculate([row[1] for row in Tab], [row[2] for row in Tab], [row[3] for row in Tab], task_number)
+    print("\npi: {} \nS: {} \nC: {} \nCq: {}".format(pi, Sj, Cj, Cjq))
+    print("Cmax: {}".format(Cmax))
+
 
 
 if __name__ == "__main__":
